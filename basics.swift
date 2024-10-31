8cb7ef528e0c6b4dffd98aaaca35817d4ccd2eff
Anuhyapeddi <42506400+Anuhyapeddi@users.noreply.github.com>
2024-10-31 12:13:31 -0400

    Update basics.swift
    declared strings and integers

--- basics.swift
+++ basics.swift
@@ -3,6 +3,102 @@
 // usage of var and let
 // var(changeable) and let(non-changeable)
 
+var name = "alive"
+name = "bob" //It is possible to change the value of the name variable
+
+let name = "charle"
+name = "dun" // it gives you an error. when we initialize with let, we cannot change its value
+
+// declaring string
+
+var name = "Alice"
+
+// multi-line string representation
+
+var movieReview = """ I Like the storyline
+of the movie, but
+I did not like the ending. """
+
+// declaring integers
+
+var num = 10
+
+var num2 = 1000000
+
+var num3 = 1000_000 // underscore is ignored by the compiler.
+
+// both num2 and num3 gives the same output 1000000
+
+// methods used by integers
+
+var number = 120
+
+print(number.isMultiple(of: 3)) // True
+
+print(number.isMultiple(of: 7)) // False
+
+//You can use the number directly 
+
+120.isMultiple(of: 3) // True
+
+// declaring decimals or floating point 
+
+var num1 = 1.0
+var num2 = 2
+var add = num1 + num2 // gives you an error, type-safe error. Cannot add integer(2) with double(1.0)
+
+//We have two ways to correct this
+
+var add = Int(num1) + num2 // convert num1 to integer(1)
+var add = num1 + Double(num2) // convertung num2 to double(2.0)
+
+// storing boolean values
+
+var value = true 
+var value2 = false
+
+var isGameOver = true // initing isGameOver is true
+isGameOver = !isGameOver //Now we have changed it to false by using !, where ! is used for not operator.
+
+var isGameOver = true 
+isGameOver.toggle() // toggle() method is used to flip the bool value
+
+// join strings
+
+//There are 2 methods to join a string
+
+// Method-1 by using '+'
+
+let part1 = "hello "
+let part2 = "world"
+let part3 = part1 +  part2  // hello world
+
+let person = "I"
+let num = 100
+let full = person + " " + "need " + String(num) + " chocolates" // I need 100 chocolates
+
+// Method-2 by using string interpolation
+
+//You can directly use any variables inside the string
+var name = "Alice"
+var job = "developer"
+var intro = "Hi!, I am \(name), I am a \(job)" // Hi!, I am Alice, I am a developer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let maximumNumberOfLoginAttempts = 10
 var currentLoginAttempt = 0
 
